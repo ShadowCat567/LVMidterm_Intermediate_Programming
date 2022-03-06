67292e3a28ec83d9d34a77c449ba3cb9c006f303
ShadowCat567 <lv2067@nyu.edu>
2022-03-05 20:23:39 -0500

    Implemented Quicksort and display of some UI
    UI = recipe fragments and start of inventory

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -19,6 +20,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Dictionary<string, int> inventory = new Dictionary<string, int>();
     List<string> recepieLst = new List<string>();
 
+    [SerializeField] GameObject InventoryPanel;
+    [SerializeField] TMP_Text recepieCounterTxt;
+    [SerializeField] TMP_Text recepieItemTxt;
+    [SerializeField] TMP_Text inventoryText;
+
     private void Awake()
     {
         playerKilled = false;
@@ -29,7 +35,10 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InventoryPanel.SetActive(false);
+        recepieCounterTxt.text = "Recepie Fragments: ( " + recepieLst.Count + "/ 5)";
+        recepieItemTxt.text = "";
+        inventoryText.text = "";
     }
 
     // Update is called once per frame
@@ -41,6 +50,28 @@ public class PlayerMovement : MonoBehaviour
         if(playerKilled == true)
         {
             //end game
+        }
+
+        if(Input.GetKey(KeyCode.Tab))
+        {
+            InventoryPanel.SetActive(true);
+            recepieCounterTxt.text = "Recepie Fragments: ( " + recepieLst.Count + "/ 5)";
+
+            Quicksort(recepieLst, 0, recepieLst.Count - 1);
+
+            recepieItemTxt.text = string.Join("\n", recepieLst);
+            
+            foreach (KeyValuePair<string, int> item in inventory)
+            {
+                inventoryText.text = item.Key + " : " + item.Value + "\n";
+            }
+            //sort dictionary and recepie list 
+            //display updated recepie and inventory 
+        }
+        
+        else
+        {
+            InventoryPanel.SetActive(false);
         }
     }
 
@@ -61,6 +92,49 @@ public class PlayerMovement : MonoBehaviour
         {
             playerKilled = true;
         }
+    }
+
+    //Used this to help figure out quicksort: https://www.geeksforgeeks.org/quick-sort/
+    void Quicksort(List<string> lst, int low, int high)
+    {
+        //while low is less than high, run quicksort
+        if (low < high)
+        {
+            //puts the partitioning index in the right place, in this case, puts the last element in the list in the right place
+            int part = Partition(lst, low, high);
+            //uses recursion to split the list based on the index returned by the partition function
+            //moves through list with recursion until there are no more values that need to be sorted
+            //first call deals with the part of the list before the partition value's index
+            Quicksort(lst, low, part - 1);
+            //second call deals with the part of the list after the partition value's index
+            Quicksort(lst, part + 1, high);
+        }
+    }
+
+    int Partition(List<string> lst, int low, int high)
+    {
+        //when given a pivot point, put all the values less than it in front of it and all values greater after
+        //pivot is value to be sorted
+        string pivot = lst[high];
+        //indicates right most position of pivot
+        int i = (low - 1);
+        for( int j = low; j <= (high - 1); j ++)
+        {
+            if(string.Compare(lst[j], pivot) < 0)
+            {
+                //if the value of j is less than the pivot, increase i and replace the value of j with the value of i 
+                i++;
+                string temp = lst[i];
+                lst[i] = lst[j];
+                lst[j] = temp;
+            }
+        }
+        //move the pivot value to its correct position
+        string temp1 = lst[i + 1];
+        lst[i + 1] = lst[high];
+        lst[high] = temp1;
+
+        return (i + 1);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
